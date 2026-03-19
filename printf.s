462226d8e444c760b8d227101971414f71dab8dd
anki <an.kiselev.2008@gmail.com>
2026-03-19 13:34:17 +0300

    added processing of %c specifier

--- printf.s
+++ printf.s
@@ -1,16 +1,42 @@
-section .text
-;------------------------------------------------------------------
-
 global _start
 
+section .text
+
+;==================================================================
+
+;------------------------------------------------------------------
+; Short:   Writes one character to stdout
+; In:      %1 --> character to write
+; Destroy: RAX, RDX, RSI, RDI
+;------------------------------------------------------------------
+
+%macro PutChar 1
+    ; rax = sys_function_code = 1 = write64()
+    mov rax, SYSCALL_CODE_WRITE
+    ; rdi = file_descriptor = stdout
+    mov rdi, STDOUT_CODE
+    ; rsi --> buffer = curr_char = first macro argument
+    mov rsi, %1
+    ; rdx = string length = 1 char
+    mov rdx, 1
+
+    syscall
+%endmacro
+
+;------------------------------------------------------------------
+
 _start:
-    mov rdi, Message
+    ; push arguments with cdecl calling convention
+    ; first argument pushed last (LIFO)
+
+    push '4'
+    push Message
 
     call Printf
 
 ; exit(0)
     ; rax = 60 = exit()
-    mov rax, 60
+    mov rax, SYSCALL_CODE_EXIT
     ; rdi = return code = 00 = success
     xor rdi, rdi
 
@@ -18,43 +44,41 @@ _start:
 
 ;-------------------<Calling Convention: cdecl>--------------------
 ; Short:   My "printf" function realisation
-; In:      RDI --> string
-;          RSI =
-;          RDX =
+; In:
 ;------------------------------------------------------------------
 
 Printf:
+    ; restore rbp value in stack
     push rbp
+    ; rbp --> stack top
     mov rbp, rsp
 
-    ; r11 = number of characters copied to buffer
-    xor r11, r11
+    ; skip pushed rbp and call address in stack to get arguments
+    add rbp, 16
+
+    ; rbx --> format string
+    mov rbx, [rbp]
+    ; rbp --> expected next argument (may not be any args)
+    add rbp, 8
 
     ; rcx = MAXIMUM_ITERATIONS
     xor rcx, rcx
     dec rcx
 
-    ; rbx --> buffer
-    mov rbx, Buffer
-
 ;------------------------------------
 .Next:
     ; if (curr_symbol == end_symbol) --> print
-    cmp byte [rdi], END_SYMBOL
+    cmp byte [rbx], END_SYMBOL
     je .Done
 
     ; if (curr_symbol == specifier_symbol)
-    cmp byte [rdi], SPEC_SYMBOL
+    cmp byte [rbx], SPEC_SYMBOL
     je .Specifier
 
-    ; store 1 char in buffer
-    mov r12, [rdi]
-    mov [rbx], r12
-    inc r11
+    ; write curr_symbol to stdout
+    PutChar rbx
 
-    ; rdi --> next char
-    inc rdi
-    ; rbx --> next char in buffer
+    ; rbx++ --> next char
     inc rbx
 
     loop .Next
@@ -63,51 +87,75 @@ Printf:
 ;------------------------------------
 
 .Specifier:
+    ; skip SPEC_SYMBOL ("%")
+    inc rbx
+
+; //TODO what to do if string ends on %
+    ; if (curr_symbol == end_symbol) --> error (?)
+    cmp byte [rbx], END_SYMBOL
+    je .Done
+
+    cmp byte [rbx], SPEC_CHAR_SYMBOL
+    je .ProcessCharSpecifier
 
     loop .Next
 ;------------------------------------
 
 .Done:
-
-; write(Buffer)
-;------------------------------------
-    ; r11 = number of characters in buffer that were filled
-    ; it exists to not write all buffer in stdout if we don't need to
-
-    ; rax = sys_function_code = 1 = write64()
-    mov rax, SYSCALL_WRITE_CODE
-    ; rdi = file_descriptor = stdout
-    mov rdi, STDOUT_CODE
-    ; rsi --> buffer
-    mov rsi, Buffer
-    ; r11 = string length
-    mov rdx, r11
-
-    syscall
-;------------------------------------
-
+    ; restore rbp value
     pop rbp
 
     ret
 
-;------------------------------------------------------------------
+;------------------------------------
+
+.ProcessCharSpecifier:
+    ; skip CHAR_SPEC_SYMBOL ("c")
+    inc rbx
+    ; write "%c" argument (they are stored in stack)
+    ; rbp --> char to write
+    PutChar rbp
+    ; rbp --> expected next argument (may not be any args)
+    add rbp, 8
+
+    loop .Next
+
+;==================================================================
 
 section .data
 ;------------------------------------------------------------------
 
-SYSCALL_WRITE_CODE  equ 0x01
+SYSCALL_CODE_WRITE  equ 1
+SYSCALL_CODE_EXIT   equ 60
 STDOUT_CODE         equ 0x01
-
-BUFFER_SIZE         equ 2048
-
-Buffer              times BUFFER_SIZE db 0
-
-LF                  equ 0x0a
 
 END_SYMBOL          equ 0x00
 SPEC_SYMBOL         equ '%'
+SPEC_CHAR_SYMBOL    equ 'c'
 
-Message             db  "salam alekum", LF
+LF                  equ 0x0a
+Message             db  "darova zaebal, ya syel %c sobak", LF, 0x00
 MessageLen          equ $ - Message
 
+CharBuffer          db 0x00
+
 ;------------------------------------------------------------------
+; BUFFER_SIZE         equ 2048
+; Buffer              times BUFFER_SIZE db 0
+;
+; ; write(Buffer)
+; ;------------------------------------
+;     ; r11 = number of characters in buffer that were filled
+;     ; it exists to not write all buffer in stdout if we don't need to
+;
+;     ; rax = sys_function_code = 1 = write64()
+;     mov rax, SYSCALL_WRITE_CODE
+;     ; rdi = file_descriptor = stdout
+;     mov rdi, STDOUT_CODE
+;     ; rsi --> buffer
+;     mov rsi, Buffer
+;     ; r11 = string length
+;     mov rdx, r11
+;
+;     syscall
+; ;------------------------------------
