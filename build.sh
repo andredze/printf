0e30ddea5b28cc20c713e1092da39e842798a033
anki <an.kiselev.2008@gmail.com>
2026-03-21 20:34:55 +0300

    added google tests for my_printf

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 
 nasm -f elf64 -l printf.lst printf.s
 
-g++ test.cpp -c test.o
+g++ test.cpp -I googletest/googletest/include -c test.o
 
-g++ -no-pie test.o printf.o -o test
+g++ test.o printf.o -no-pie googletest/build/lib/libgtest.a -o test
 
 # ld -melf_x86_64 --dynamic-linker=/lib64/ld-linux-x86-64.so.2 printf.o -lc -o printf
