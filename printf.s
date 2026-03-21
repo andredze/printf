19654ae980e3b4946743cb33cf9a381dd131491c
anki <an.kiselev.2008@gmail.com>
2026-03-21 18:32:20 +0300

    added %d

--- printf.s
+++ printf.s
@@ -39,40 +39,40 @@ extern printf
 %endmacro
 
 ;------------------------------------------------------------------
-
-_start:
-    ; push arguments with cdecl calling convention
-    ; first argument pushed last (LIFO)
-
-    push 256
-    push 16
-    push 256
-    push '4'
-    push Message
-
-    call my_printf
-
-;------------------------------------
-
-    mov rdi, Message
-    mov rsi, '4'
-    mov rdx, 256
-    mov rcx, 16
-    mov r8, 256
-
-    xor rax, rax
-
-    call printf
-
-;------------------------------------
-
-; exit(0)
-    ; rax = 60 = exit()
-    mov rax, SYSCALL_CODE_EXIT
-    ; rdi = return code = 00 = success
-    xor rdi, rdi
-
-    syscall
+;
+; _start:
+;     ; push arguments with cdecl calling convention
+;     ; first argument pushed last (LIFO)
+;
+;     push 256
+;     push 16
+;     push 256
+;     push '4'
+;     push Message
+;
+;     call my_printf
+;
+; ;------------------------------------
+;
+;     mov rdi, Message
+;     mov rsi, '4'
+;     mov rdx, 256
+;     mov rcx, 16
+;     mov r8, 256
+;
+;     xor rax, rax
+;
+;     call printf
+;
+; ;------------------------------------
+;
+; ; exit(0)
+;     ; rax = 60 = exit()
+;     mov rax, SYSCALL_CODE_EXIT
+;     ; rdi = return code = 00 = success
+;     xor rdi, rdi
+;
+;     syscall
 
 ;------------------<Calling Convention: stdcall>-------------------
 ; Note:    System V ABI for x86-64
@@ -105,13 +105,11 @@ my_printf:
     pop r8
     pop r9
 
-    ; restore call address
-    push r15
-
     xor rax, rax
     call printf
 
-    ret
+    ; restore call address
+    jmp r15
 
 ;-------------------<Calling Convention: cdecl>--------------------
 ; Short:   My "printf" function realisation
@@ -143,7 +141,7 @@ Next:
     je Done
 
     ; if (curr_symbol == specifier_symbol)
-    cmp byte [rbx], SPEC_SYMBOL
+    cmp byte [rbx], SPEC_SYMBOL_START
     je Specifier
 
     ; write curr_symbol to stdout
@@ -153,34 +151,6 @@ Next:
     inc rbx
 
     loop Next
-    jmp Done
-
-;------------------------------------
-
-Specifier:
-    ; skip SPEC_SYMBOL ("%")
-    inc rbx
-
-; //TODO what to do if string ends on %
-    movzx r13, byte [rbx]
-    sub r13, SPEC_BIN_SYMBOL
-    mov r14, [SpecifiersJumpTable + r13 * 8]
-    jmp r14
-
-;     cmp byte [rbx], SPEC_CHAR_SYMBOL
-;     je .ProcessCharSpecifier
-;
-;     cmp byte [rbx], SPEC_HEX_SYMBOL
-;     je .ProcessHexSpecifier
-;
-;     cmp byte [rbx], SPEC_OCT_SYMBOL
-;     je .ProcessOctSpecifier
-;
-;     cmp byte [rbx], SPEC_BIN_SYMBOL
-;     je .ProcessBinSpecifier
-
-    loop Next
-;------------------------------------
 
 Done:
     ; restore rbp value
@@ -190,37 +160,116 @@ Done:
 
 ;------------------------------------
 
-ProcessWrongSpecifier:
-ProcessCharSpecifier:
-    ; skip CHAR_SPEC_SYMBOL ("c")
+Specifier:
+    ; skip SPEC_SYMBOL ("%")
     inc rbx
+
+    ; r13 = specifier symbol character
+    movzx r13, byte [rbx]
+    ; skip specifier symbol character
+    inc rbx
+
+    ; if char repeats the SPEC_SYMBOL --> it was escaped
+    cmp byte r13b, SPEC_SYMBOL_START
+    je ProcessSpecifierWrong
+
+    ; check the jump table bounds
+    ; if (char < first specifier) --> wrong
+    cmp byte r13b, SPEC_SYMBOL_BIN
+    jl ProcessSpecifierWrong
+
+    ; if (char > last specifier) --> wrong
+    cmp byte r13b, SPEC_SYMBOL_HEX
+    jg ProcessSpecifierWrong
+    ; jump to ProcessSpecifier by the letter
+    ; - SPEC_SYMBOL_BIN * 8 to get the distance from
+    ; SPEC_SYMBOL_BIN character (first specifier)
+    ; * 8 as pointers are stored with 8 bytes (64 bit architecture)
+    jmp [SpecifiersJumpTable - SPEC_SYMBOL_BIN * 8 + r13 * 8]
+
+;------------------------------------
+
+ProcessSpecifierWrong:
+    ; just print the SPEC_SYMBOL_START
+    ; if it was escaped or it was the wrong spec
+    ; (as default libc print does that)
+    PutChar SpecSymbolStart
+
+    loop Next
+
+;------------------------------------
+
+ProcessSpecifierChar:
     ; write "%c" argument (they are stored in stack)
     ; rbp --> char to write
     PutChar rbp
     ; rbp --> expected next argument (may not be any args)
     add rbp, 8
 
-    loop Next
+    dec rcx
+    jnz Next
 
 ;------------------------------------
 
-ProcessHexSpecifier:
+ProcessSpecifierString:
+    ; r12 --> string to print
+    mov r12, [rbp]
+    ; rbp --> expected next argument (may not be any args)
+    add rbp, 8
+
+    ; r11 = MAX_LOOP_ITERS
+    mov r11, -1
+
+NextPutChar:
+    ; if (char == END_SYMBOL) --> done
+    cmp byte [r12], END_SYMBOL
+    je ProcessStringDone
+
+    ; else --> PutChar
+    PutChar r12
+    ; go to the next char
+    inc r12
+
+    dec r11
+    jnz NextPutChar
+
+ProcessStringDone:
+
+    dec rcx
+    jnz Next
+
+;------------------------------------
+
+ProcessSpecifierDec:
+    ; rax = argument integer
+    mov rax, [rbp]
+    ; rbp --> expected next argument (may not be any args)
+    add rbp, 8
+
+    call ConvertDecimalToAscii
+
+    dec rcx
+    jnz Next
+
+;------------------------------------
+
+ProcessSpecifierHex:
     ; 2**4 = 16 -- degree of hex num system
     mov r12, 4
 
-    jmp ConvertInteger
+    jmp ConvertPowerOfTwoToAscii
 
 ;------------------------------------
 
-ProcessOctSpecifier:
+ProcessSpecifierOct:
     ; 2**3 = 8 -- degree of oct num system
     mov r12, 3
 
-    jmp ConvertInteger
+    jmp ConvertPowerOfTwoToAscii
 
 ;------------------------------------
 
-ProcessBinSpecifier:
+ProcessSpecifierBin:
     ; 2**1 = 2 -- degree of bin num system
     mov r12, 1
 
@@ -228,18 +277,13 @@ ProcessBinSpecifier:
 
 ;------------------------------------
 
-ConvertInteger:
-
-    ; skip BIN_SPEC_SYMBOL ("b")
-    inc rbx
-    ; write "%b" argument (they are stored in stack)
-
-    ; r10 = argument (integer) to make bin
+ConvertPowerOfTwoToAscii:
+    ; r10 = argument integer
     mov r10, [rbp]
     ; rbp --> expected next argument (may not be any args)
     add rbp, 8
 
-    call PrintConvertedInteger
+    call PrintConvertedPowerOfTwoToAscii
 
     dec rcx
     jnz Next
@@ -251,7 +295,7 @@ ConvertInteger:
 ; Destroy: R10, R11
 ;------------------------------------------------------------------
 
-PrintConvertedInteger:
+PrintConvertedPowerOfTwoToAscii:
     push rcx
 
     ; get r14 = mask for getting lowest part of number
@@ -265,7 +309,7 @@ PrintConvertedInteger:
     dec r14
 
     ; r13 used for indexing buffer
-    mov r13, IntBufferSize - 1
+    mov r13, INT_BUFFER_SIZE - 1
 
 .NextByte:
     ; copy to r11
@@ -302,13 +346,91 @@ PrintConvertedInteger:
     add r13, IntBuffer + 1
 
     ; string length = end buffer ptr - start buffer ptr
-    mov rcx, IntBuffer + IntBufferSize
+    mov rcx, IntBuffer + INT_BUFFER_SIZE
     sub rcx, r13
 
     ; when ended --> print buffer
     PutStr r13, rcx
 
     pop rcx
+
+    ret
+
+;------------------------------------------------------------------
+; Short:   -
+; Exp:     -
+; In:      -
+; Out:     -
+; Destroy: -
+;------------------------------------------------------------------
+
+ConvertDecimalToAscii:
+
+    ; check int sign
+    cmp rax, 0
+    jge .DoneWithSign
+
+    ; if negative --> print '-' and convert to positive
+    ; print '-'
+    PutChar MinusSign
+
+    ; convert to positive
+    neg rax
+
+.DoneWithSign:
+
+    ; r14 will be used for indexing buffer
+    mov r14, IntBuffer
+
+    ; r13 = MAX_LOOP_ITERS
+    mov r13, -1
+
+.NextDigit:
+
+    ; if (number == 0) --> end
+    cmp rax, 0
+    jle .Done
+
+    ; r11 = divisor = 10
+    mov r11, 10
+
+    ; div divides rax by argument:
+    ; r11 = divisor = 10
+    ; div is putting
+    ; rax = quotient
+    ; rdx = dividend = 0
+    xor rdx, rdx
+    div r11
+    ; so, rax //= 10
+    ; rdx = next digit to put
+
+    ; convert digit to ascii
+    add rdx, '0'
+
+    ; store char in IntBuffer in reversed order
+    mov byte [r14], dl
+
+    ; go to storing next char (r14++)
+    inc r14
+
+    dec r13
+    jnz .NextDigit
+
+.Done:
+
+.PrintNext:
+    ; print int buffer from the end
+    ; go to last digit
+    dec r14
+
+    cmp r14, IntBuffer
+    jl .DonePrinting
+
+    PutChar r14
+
+    jmp .PrintNext
+
+.DonePrinting:
 
     ret
 
@@ -322,47 +444,50 @@ SYSCALL_CODE_EXIT   equ 60
 STDOUT_CODE         equ 0x01
 
 END_SYMBOL          equ 0x00
-SPEC_SYMBOL         equ '%'
-SPEC_CHAR_SYMBOL    equ 'c'
-SPEC_HEX_SYMBOL     equ 'x'
-SPEC_OCT_SYMBOL     equ 'o'
-SPEC_BIN_SYMBOL     equ 'b'
-SPEC_DEC_SYMBOL     equ 'd'
-SPEC_STR_SYMBOL     equ 's'
+SPEC_SYMBOL_START   equ '%'
+SPEC_SYMBOL_CHAR    equ 'c'
+SPEC_SYMBOL_HEX     equ 'x'
+SPEC_SYMBOL_OCT     equ 'o'
+SPEC_SYMBOL_BIN     equ 'b'
+SPEC_SYMBOL_DEC     equ 'd'
+SPEC_SYMBOL_STR     equ 's'
 
-SpecifiersJumpTable dq ProcessBinSpecifier      ; 'b'
-                    dq ProcessCharSpecifier     ; 'c'
-                    ; db ProcessDecSpecifier    ; 'd'
-                    dq ProcessWrongSpecifier    ; 'd'
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessOctSpecifier      ; 'o'
-                    dq ProcessHexSpecifier      ; 'p'
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier    ; 's'
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessWrongSpecifier
-                    dq ProcessHexSpecifier      ; 'x'
+MinusSign           db '-'
+SpecSymbolStart     db '%'
 
-LF                  equ 0x0a
-Message             db  "darova zaebal, ya syel %c sobak; hex 52 = %p; oct 16 = %o; bin 256 = %b", LF, 0x00
-MessageLen          equ $ - Message
+SpecifiersJumpTable dq ProcessSpecifierBin      ; 'b'
+                    dq ProcessSpecifierChar     ; 'c'
+                    dq ProcessSpecifierDec      ; 'd'
+                    dq ProcessSpecifierWrong    ; 'e'
+                    dq ProcessSpecifierWrong    ; 'f'
+                    dq ProcessSpecifierWrong    ; 'g'
+                    dq ProcessSpecifierWrong    ; 'h'
+                    dq ProcessSpecifierWrong    ; 'j'
+                    dq ProcessSpecifierWrong    ; 'i'
+                    dq ProcessSpecifierWrong    ; 'k'
+                    dq ProcessSpecifierWrong    ; 'l'
+                    dq ProcessSpecifierWrong    ; 'm'
+                    dq ProcessSpecifierWrong    ; 'n'
+                    dq ProcessSpecifierOct      ; 'o'
+                    dq ProcessSpecifierHex      ; 'p'
+                    dq ProcessSpecifierWrong    ; 'q'
+                    dq ProcessSpecifierWrong    ; 'r'
+                    dq ProcessSpecifierString   ; 's'
+                    dq ProcessSpecifierWrong    ; 't'
+                    dq ProcessSpecifierWrong    ; 'u'
+                    dq ProcessSpecifierWrong    ; 'v'
+                    dq ProcessSpecifierWrong    ; 'w'
+                    dq ProcessSpecifierHex      ; 'x'
+
+DigitBuffer         db '0'
 
 IntBuffer           times 64 db 0x00
-IntBufferSize       equ $ - IntBuffer
+INT_BUFFER_SIZE     equ $ - IntBuffer
 
 ;------------------------------------------------------------------
+; LF                  equ 0x0a
+; Message             db  "darova zaebal, ya syel %c sobak; hex 52 = %p; oct 16 = %o; bin 256 = %b", LF, 0x00
+; MessageLen          equ $ - Message
 ; BUFFER_SIZE         equ 2048
 ; Buffer              times BUFFER_SIZE db 0
 ;
